a99edb2c6a30be0028f0d88f36b85cee6c026ae1
Andrea Allen <andrea2allen@gmail.com>
2020-10-05 11:26:14 -0400

    Rename files, clean up, remove comments, add documentation

--- SIS_deterministic.py
+++ SIS_deterministic.py
@@ -1,8 +1,7 @@
-import euler_steps
+import numerical_integrators
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
 
 def SIS_euler(N, gamma, beta, step_size_h, steps):
     S_current = 90
@@ -11,26 +10,17 @@ def SIS_euler(N, gamma, beta, step_size_h, steps):
     I_vec=[I_current]
     time = 0
     time_vec = [0]
-    # try:
     for t in range(1, math.floor(steps/step_size_h)):
             dI_dt_current = dI_dt(S_current, I_current, gamma, beta)
-            I_plus_t = euler_steps.euler_step(step_size_h, I_current, dI_dt_current)
+            I_plus_t = numerical_integrators.euler_step(step_size_h, I_current, dI_dt_current)
             I_vec.append(I_plus_t)
             S_vec.append(N-I_plus_t)
             S_current=S_vec[-1]
             I_current=I_vec[-1]
             time += step_size_h
             time_vec.append(time)
-    # except OverflowError:
-    #     if len(I_vec) > len(time_vec):
-    #         I_vec.pop(-1)
-    # if (True in np.isinf(I_vec)):
-    #     I_vec = I_vec[:np.where(np.isinf(I_vec))[0][0]]
-    # if (True in np.isnan(I_vec)):
-    #     I_vec = I_vec[:np.where(np.isnan(I_vec))[0][0]]
 
     time_vec = time_vec[:len(I_vec)]
-    # plt.scatter(time_vec, I_vec, label='Euler')
     plt.plot(time_vec, I_vec, alpha=0.6, lw=2, label='Euler')
     plt.legend(loc='lower right')
     print(I_vec)
@@ -45,13 +35,15 @@ def SIS_heun(N, gamma, beta, step_size_h, steps):
     time = 0
     time_vec = [0]
     difference_vec = [0]
-    # try:
     for t in range(1, math.floor(steps/step_size_h)):
             dI_dt_current = dI_dt(S_current, I_current, gamma, beta)
-            I_t_plus_h_eu = euler_steps.euler_step(step_size_h, I_current, dI_dt_current)
+            # Euler step for x(t):
+            I_t_plus_h_eu = numerical_integrators.euler_step(step_size_h, I_current, dI_dt_current)
             S_t_plus_h_eu = N - I_t_plus_h_eu
+            # dx/dt of Euler step:
             dI_dt_Eu_t_plus_h = dI_dt(S_t_plus_h_eu, I_t_plus_h_eu, gamma, beta)
-            I_t_plus_h_Hu = euler_steps.heun_step(step_size_h, I_current, dI_dt_current, dI_dt_Eu_t_plus_h)
+            # Heun step:
+            I_t_plus_h_Hu = numerical_integrators.heun_step(step_size_h, I_current, dI_dt_current, dI_dt_Eu_t_plus_h)
             I_vec.append(I_t_plus_h_Hu)
             S_vec.append(N - I_t_plus_h_Hu)
             S_current=S_vec[-1]
@@ -59,20 +51,8 @@ def SIS_heun(N, gamma, beta, step_size_h, steps):
             time += step_size_h
             time_vec.append(time)
             difference_vec.append(abs(I_vec[-1]-I_vec_E[-1]))
-    # except TypeError or OverflowError:
-    #     if len(I_vec_E)>len(time_vec):
-    #         I_vec_E.pop(-1)
-    #     if len(I_vec) > len(time_vec):
-    #         I_vec.pop(-1)
-    # if (True in np.isinf(I_vec)):
-    #     I_vec = I_vec[:np.where(np.isinf(I_vec))[0][0]]
-    # if (True in np.isnan(I_vec)):
-    #     I_vec = I_vec[:np.where(np.isnan(I_vec))[0][0]]
-    # if (step_size_h==2):
-    #     print(step_size_h)
     time_vec = time_vec[:len(I_vec)]
     print(I_vec)
-    # plt.scatter(time_vec, I_vec, label='Heun')
     plt.plot(time_vec, I_vec, alpha=0.6, lw=2, label='Heun')
     plt.legend(loc='lower right')
     plt.xlabel('Time t=0 to T=50/h')
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
-import euler_steps
+import numerical_integrators
 import SIS_deterministic
 import matplotlib.pyplot as plt
 
 def run_integrators(beta, step_size_h, log=False, save=False):
-    heun_vec = SIS_deterministic.SIS_heun(100, 0.25, beta, step_size_h, 50)
-    euler_vec = SIS_deterministic.SIS_euler(100, 0.25, beta, step_size_h, 50)
+    SIS_deterministic.SIS_heun(100, 0.25, beta, step_size_h, 50)
+    SIS_deterministic.SIS_euler(100, 0.25, beta, step_size_h, 50)
     plt.plot([], [], ' ', label="Beta="+str(beta)+", h="+str(step_size_h))
     plt.legend(loc='lower right')
-    if log==True:
+    if log:
         plt.loglog()
     if save:
         plt.savefig('euler_heun_'+str(beta)+str(step_size_h)+'.png')
@@ -18,50 +18,12 @@ if __name__ == '__main__':
     print("MOCS Assignment 1. \nBy Andrea Allen and Amanda Bertschinger")
     run_integrators(0.03, 0.01, True, True)
     run_integrators(0.06, 0.01, True, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.06, 0.01, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.06, 0.01, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.06, h=.01,")
-    # plt.show()
     run_integrators(0.1, 0.01, True, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.1, 0.01, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.1, 0.01, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.1, h=.01,")
-    # plt.show()
+
     run_integrators(0.03, 0.5, True, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.03, 0.5, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.03, 0.5, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.03, h=.5,")
-    # plt.show()
     run_integrators(0.06, 0.5, False, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.06, 0.5, 50) #Euler chaos?
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.06, 0.5, 50) #Euler chaos?
-    # plt.loglog()
-    # plt.title("Beta=0.06, h=.5,")
-    # plt.show()
     run_integrators(0.1, 0.5, False, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.1, 0.5, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.1, 0.5, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.1, h=.5,")
-    # plt.show()
+
     run_integrators(0.03, 2.0, False, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.03, 2.0, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.03, 2.0, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.03, h=2,")
-    # plt.show()
     run_integrators(0.06, 2.0, False, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.06, 2.0, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.06, 2.0, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.06, h=2,")
-    # plt.show()
     run_integrators(0.1, 2.0, False, True)
-    # euler_vec = SIS_deterministic.SIS_euler(100, 0.25, 0.1, 2.0, 50)
-    # heun_vec = SIS_deterministic.SIS_heun(100, 0.25, 0.1, 2.0, 50)
-    # plt.loglog()
-    # plt.title("Beta=0.1, h=2,")
-    # plt.show()
